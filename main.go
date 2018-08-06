0a5b3833bc2c83628ed8ecee14548feda4564f9a
maxime.perrimond <maxime.perrimond@zenport.io>
2018-08-06 15:56:49 +0900

    Initial commit

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"os"
+	"os/signal"
+	"syscall"
+
+	"gitlab.com/zenport.io/go-assignment/adapters/http"
+	"gitlab.com/zenport.io/go-assignment/engine"
+	"gitlab.com/zenport.io/go-assignment/providers/database"
+)
+
+func main() {
+	provider := database.NewProvider()
+	e := engine.NewEngine(provider)
+
+	adapter := http.NewHTTPAdapter(e)
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
+	defer close(stop)
+
+	adapter.Start()
+
+	<-stop
+
+	adapter.Stop()
+	provider.Close()
+}
---
+++ providers/database/knight.go
@@ -0,0 +1,9 @@
+package database
+
+import "gitlab.com/zenport.io/go-assignment/domain"
+
+type knightRepository struct{}
+
+func (repository *knightRepository) Find(ID string) *domain.Knight {
+	return nil
+}
